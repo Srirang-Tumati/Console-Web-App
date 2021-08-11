890acb9407afb8290177cad1c7724cc8df269053
Srirang-Tumati <81486649+Srirang-Tumati@users.noreply.github.com>
2021-08-11 09:23:22 -0700

    Add files via upload

---
+++ myconsole.js
@@ -0,0 +1,11 @@
+function myconsole()
+{
+    var a = 10;
+    var b = 5;
+    var add = a + b;
+    console.log(add);
+    var first_name = "Ranbir";
+    var last_name = "Kapoor";
+    var res = first_name.concat(last_name);
+    console.log(res);
+}
